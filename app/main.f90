566759743f07c8afe8c0dc5b65f05e694d7a3e8f
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-12-15 02:20:31 -0500

    project

--- app/main.f90
+++ app/main.f90
@@ -1,6 +1,16 @@
 program main
   use :: glfw
+  use :: opengl
   implicit none
 
+
+  if (.not. glfw_init()) then
+    error stop "Failed to init glfw."
+  end if
+
+
+
+
+  call glfw_terminate()
 
 end program main
