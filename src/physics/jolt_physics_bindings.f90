faa35b29a197f9e0002a6bbf1e811e8180985aa8
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-12-15 11:28:41 -0500

    wow

--- src/physics/jolt_physics_bindings.f90
+++ src/physics/jolt_physics_bindings.f90
@@ -2,10 +2,12 @@ module jolt_physics_bindings
   use, intrinsic :: iso_c_binding
   implicit none
 
+
   interface
 
 
-  
+    subroutine print_hi() bind(c, name = "print_hi")
+    end subroutine print_hi
 
 
   end interface
