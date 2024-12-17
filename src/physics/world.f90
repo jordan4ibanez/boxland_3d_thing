dc5d2b2fbea087831a189c8fed7c3e58c9d8c768
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-12-17 05:02:14 -0500

    magical

--- src/physics/world.f90
+++ src/physics/world.f90
@@ -9,9 +9,12 @@ module world
   ! So I literally have no idea what I'm doing.
 
 
-  integer(c_int16_t), parameter :: NON_MOVING = 0
-  integer(c_int16_t), parameter :: MOVING = 1
+  integer(c_int16_t), parameter :: LAYERS_NON_MOVING = 0
+  integer(c_int16_t), parameter :: LAYERS_MOVING = 1
   integer(c_int32_t), parameter :: NUM_LAYERS = 2
+
+  integer(c_int8_t), parameter :: BROAD_PHASE_LAYERS_NON_MOVING = 0
+  integer(c_int8_t), parameter :: BROAD_PHASE_LAYERS_MOVING = 1
 
 
   type(c_ptr) :: job_system_threadpool
@@ -38,12 +41,13 @@ contains
     ! We're only using 2 layers.
     ! One for non-moving, one for moving.
     object_layer_pair_filter_table = jph_object_layer_pair_filter_table_create(2)
-    call jph_object_layer_pair_filter_table_enable_collision(object_layer_pair_filter_table, NON_MOVING, MOVING)
-    call jph_object_layer_pair_filter_table_enable_collision(object_layer_pair_filter_table, MOVING, NON_MOVING)
+    call jph_object_layer_pair_filter_table_enable_collision(object_layer_pair_filter_table, LAYERS_NON_MOVING, LAYERS_MOVING)
+    call jph_object_layer_pair_filter_table_enable_collision(object_layer_pair_filter_table, LAYERS_MOVING, LAYERS_NON_MOVING)
 
     ! We use a 1 to 1 mapping between object layers and broadphase layers.
     broad_phase_layer_interface_table = jph_broad_phase_layer_interface_table_create(2, 2)
-
+    call jph_broadphaselayer_interfacetable_map_object_tobroadphaselayer(broad_phase_layer_interface_table, LAYERS_NON_MOVING, BROAD_PHASE_LAYERS_NON_MOVING)
+    call jph_broadphaselayer_interfacetable_map_object_tobroadphaselayer(broad_phase_layer_interface_table, LAYERS_MOVING, BROAD_PHASE_LAYERS_MOVING)
 
     print"(A)","World created."
   end subroutine world_create
