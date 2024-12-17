1e20f9e33d2879bdf9b852abbacfb0d48b71bb29
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-12-17 05:35:51 -0500

    AND THIS

--- src/physics/jolt_bindings.f90
+++ src/physics/jolt_bindings.f90
@@ -5,46 +5,6 @@ module jolt_bindings
 ! Copyright (c) Amer Koleci and Contributors.
 ! Licensed under the MIT License (MIT). See LICENSE in the repository root for more information.
 
-! #ifndef JOLT_C_H_
-! #define JOLT_C_H_ 1
-
-! #if defined(JPH_SHARED_LIBRARY_BUILD)
-! #   if defined(_MSC_VER)
-! #       define _JPH_EXPORT __declspec(dllexport)
-! #   elif defined(__GNUC__)
-! #       define _JPH_EXPORT __attribute__((visibility("default")))
-! #   else
-! #       define _JPH_EXPORT
-! #       pragma warning "Unknown dynamic link import/export semantics."
-! #   endif
-! #elif defined(VIMAGE_SHARED_LIBRARY_INCLUDE)
-! #   if defined(_MSC_VER)
-! #       define _JPH_EXPORT __declspec(dllimport)
-! #   else
-! #       define _JPH_EXPORT
-! #   endif
-! #else
-! #   define _JPH_EXPORT
-! #endif
-
-! #ifdef __cplusplus
-! #    define _JPH_EXTERN extern "C"
-! #else
-! #    define _JPH_EXTERN extern
-! #endif
-
-! #ifdef _WIN32
-! #   define JPH_API_CALL __cdecl
-! #else
-! #   define JPH_API_CALL
-! #endif
-
-! #define JPH_CAPI _JPH_EXTERN _JPH_EXPORT
-
-! #include <stdbool.h>
-! #include <stdint.h>
-! #include <stddef.h>
-
 real(c_float), parameter :: JPH_DEFAULT_COLLISION_TOLERANCE = real(1.0e0 - 4.0) ! float cDefaultCollisionTolerance = 1.0e-4f
 #define JPH_DEFAULT_PENETRATION_TOLERANCE (1.0e-4f) ! float cDefaultPenetrationTolerance = 1.0e-4f
 #define JPH_DEFAULT_CONVEX_RADIUS (0.05f) ! float cDefaultConvexRadius = 0.05f
